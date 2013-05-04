822ce0d1e42e1b455c05d1f1f879b950225b0915
Andraz Bajt <edofic@gmail.com>
2013-05-04 14:57:56 +0200

    buggy camera manipulation

--- main.cpp
+++ main.cpp
@@ -13,7 +13,7 @@ using namespace video;
 #include "Camera.h"
 #include "Renderer.h"
 
-#define W 350
+#define W 200
 #define H 200
 
 int main()
@@ -24,6 +24,10 @@ int main()
 
 	ITexture* rtt = driver->addTexture(dimension2d<u32>(W,H), "rtt");
 	Camera cam(vector3df(3, 6, 6), vector3df(0,0,0));
+	ICameraSceneNode* irrCam = smgr->addCameraSceneNodeFPS();
+	irrCam->setTarget(cam.getLookat());
+	irrCam->setUpVector(cam.getUpVector());
+	irrCam->setPosition(cam.getPosition());
 	Renderer r(rtt, &cam);
 
 	Material* basicMat = new Material(0.3f, 0.f,
@@ -56,71 +60,10 @@ int main()
 		r.renderScene(&scn);
 
 		driver->beginScene();
+		cam.setPosition(irrCam->getPosition());
+    cam.setLookat(irrCam->getTarget()); 
 		driver->draw2DImage(rtt, vector2d<s32>());
+		smgr->drawAll();
 		driver->endScene();
 	}
-
-	/*ray r;
-	r.direction.X = 1;
-
-	SSphere s(vector3df(10,0,0), 2);
-	//SPlane s(vector3df(10,0,0), vector3df(-1,1,1));
-	core::vector3df light(5,3,5);
-
-	ITexture* rtt = driver->addTexture(dimension2d<u32>(W,H), "rtt");
-
-	SColor col;
-	vector3df inter;
-
-	float phi = 0;
-	unsigned int counter = 0;
-	while (device->run())
-	{
-		if (device->isWindowActive())
-		{
-			float dt = device->getTimer()->getSpeed();
-
-			phi += dt * 0.1f;
-			light.set(5, 3*sin(phi), 5*cos(phi));
-			s.setCenter(vector3df(10+sin(phi*0.5f),
-				0.2f*cos(phi*0.4f),
-				cos(phi*0.7f)));
-
-			// raytrace
-			int* data = (int*)rtt->lock();
-			for (int i=0; i<W; i++)
-			{
-				for (int j=0; j<H; j++)
-				{
-					r.direction.Y = (i - W/2) * 0.01f;
-					r.direction.Z = (j - H/2) * 0.01f;
-					bool isInter = s.getIntersection(r, inter);
-					if (!isInter)
-						col.set(255, 255,255,255);
-					else
-					{
-						float d = s.getNormal(inter).dotProduct((light - inter).normalize());
-						if (d < 0)
-							d = 0;
-						col.set(255, 255*d,127*d,39*d);
-					}
-					data[i+W*j] = col.color;
-				}
-			}
-			rtt->unlock();
-
-			// render
-
-			driver->beginScene(true, true, SColor(255, 255,255,255));
-			smgr->drawAll();
-			driver->draw2DImage(rtt, vector2d<s32>());
-			driver->endScene();
-
-			stringw caption;
-			caption += counter++;
-			device->setWindowCaption(caption.c_str());
-		}
-		else
-			device->yield();
-	}*/
 }
